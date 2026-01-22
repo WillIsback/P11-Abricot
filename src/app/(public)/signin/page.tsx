aeda54ae31f6d50fb4978395bd2974aa5d7a29c5
william derue <william.derue@gmail.com>
2026-01-22 18:30:12 +0100

    feat: add login and signin pages with custom components
    - Created a new login page with email and password inputs, a login button, and a link for forgotten passwords.
    - Created a new signin page with similar structure, allowing users to create an account.
    - Implemented a home page showcasing various UI components including buttons, inputs, and task management features.

---
+++ src/app/(public)/signin/page.tsx
@@ -0,0 +1,52 @@
+import LogoBrandDark from "@/assets/logo/loge_brand_dark.svg";
+import CustomInput from "@/components/CustomInput/CustomInput";
+import CustomButton from "@/components/CustomButton/CustomButton";
+import CustomLink from "@/components/CustomLink/CustomLink";
+
+export default function SignIn() {
+  return (
+    <div className="flex h-screen">
+      <div className="flex m-auto h-256 w-360 bg-[url('/images/SignIn.png')] bg-contain bg-center">
+        <main className="flex items-center justify-center w-2/5 h-256 flex-col px-35 py-13.75 gap-50.5 relative z-10 bg-gray-50">
+          <LogoBrandDark />
+          <div className="flex flex-col gap-7.5  justify-center items-center">
+            <h1 className="text-brand-dark">Inscription</h1>
+            <form className="flex flex-col gap-7.25 items-center">
+              <div className="flex flex-col gap-7.25">
+                <CustomInput
+                  label="Email"
+                  type="email"
+                  inputID="mail"
+                />
+                <CustomInput
+                  label="Mot de passe"
+                  type="password"
+                  inputID="mail"
+                />
+              </div>
+              <div className="flex w-249/282">
+                <CustomButton
+                  label='Se connecter'
+                  pending={false}
+                  disabled={false}
+                  buttonType= "submit"
+                />
+              </div>
+            </form>
+            <CustomLink
+              label="Mot de passe oublié ?"
+              link="#contact"
+            />
+          </div>
+          <div className="flex whitespace-nowrap gap-2.5 justify-center items-center">
+            <p className="body-s">Déjà inscrit ?</p>
+            <CustomLink
+              label="Se connecter"
+              link="/login"
+            />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
--- src/components/CustomInput/CustomInput.tsx
+++ src/components/CustomInput/CustomInput.tsx
@@ -1,18 +1,18 @@
 import DatePicker from './DatePicker/DatePicker';
 import { ComboBox } from './ComboBox/ComboBox';
 
-type InputType = 'Default' | 'DatePicker' | 'ComboBox';
+type InputType = 'DatePicker' | 'ComboBox' | 'email' | 'password' | 'text';
 
 
 
 export default function CustomInput ({ label, inputID, type } : {label: string, inputID: string, type: InputType}){
     return (
-        <div className="w-70 h-fit flex flex-col ">
+        <div className="w-70 h-fit flex flex-col gap-1.75">
             <label className="body-s " htmlFor={inputID}>
                 {label}
             </label>
-            {type === 'Default' 
-                ? <input id={inputID} type='text' className="rounded-s min-h-13.25 bg-white body-xs focus:outline-none px-4.25 py-4.75"/>
+            {(type === 'text' || 'email' || 'password')
+                ? <input id={inputID} type={type} className="rounded-s min-h-13.25 bg-white body-xs focus:outline-none px-4.25 py-4.75"/>
                 : type === 'DatePicker'
                     ? <DatePicker/>
                     : <ComboBox />
